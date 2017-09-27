a242c949a39242e7da60307736d8a6d41f3c2a96
Dan Feeney <daniel.feeney@colorado.edu>
2017-09-27 13:41:24 -0600

    Update Bikeanalysis.py
    This will read in data, make a target of true/false if an overcharge occurred. Then fits a logistic regression with 3 variables. It seems to run

--- Bikeanalysis.py
+++ Bikeanalysis.py
@@ -1,24 +1,33 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import csv
-from sklearn import LogisticRegression
-%matplotlib inline
+from sklearn import linear_model
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 
 #Read in the dataframe
 df = pd.read_csv('sheet_checkouts_by_station_map_data.csv')
-df.head(5)
 
 #Get all trips longer than 30 mins and the type is not equal to annual. Call it 'Overcharge'
-df2 = (df['Trip Duration (Minutes)'] > 30) & (df['Entry Pass Type'] !='Annual')\
-        | df['Trip Duration (Minutes)'] > 30) & (df['Entry Pass Type'] != 'Student')\
-        | (df['Trip Duration (Minutes)'] > 60) & (df['Entry Pass Type'] == 'Annual')\
-        | (df['Trip Duration (Minutes)'] > 60) & (df['Entry Pass Type'] == 'Student')
+#Get all trips longer than 30 mins and the type is not equal to annual
+df2 = pd.DataFrame((df['Trip Duration (Minutes)'] > 30) & (df['Entry Pass Type'] !='Annual')\
+|(df['Trip Duration (Minutes)'] > 30) & (df['Entry Pass Type'] != 'Student')\
+|(df['Trip Duration (Minutes)'] > 60) & (df['Entry Pass Type'] == 'Annual')\
+|(df['Trip Duration (Minutes)'] > 60) & (df['Entry Pass Type'] == 'Student'))
 
 df['Overcharge'] = df2
+# Initialize label encoder
+label_encoder = preprocessing.LabelEncoder()
 
+# Convert day of week variable to numeric
+encoded_day = label_encoder.fit_transform(df["Checkout Day of Week"])
 
-# Convert some possible predictor variables to numeric
+# Initialize logistic regression model
+log_model = linear_model.LogisticRegression()
+
+#Try with more variables
+
+# Convert more variables to numeric
 encoded_day = label_encoder.fit_transform(df["Checkout Day of Week"])
 encoded_cstation = label_encoder.fit_transform(df["Checkout Station (Station Information)"])
 encoded_rstation = label_encoder.fit_transform(df["Return Station (Station Information)"])
@@ -26,16 +35,15 @@ encoded_rstation = label_encoder.fit_transform(df["Return Station (Station Infor
 train_features = pd.DataFrame([encoded_cstation,
                               encoded_rstation,
                               encoded_day]).T
-
 #Do a train/test split
 X_train, X_test, y_train, y_test = train_test_split(train_features, df['Overcharge'], test_size=0.4, random_state=0)
-
 # Initialize logistic regression model
 log_model = linear_model.LogisticRegression()
 
 # Train the model
 log_model.fit(X = X_train ,
               y = y_train)
+
 
 # Check trained model intercept
 print(log_model.intercept_)
